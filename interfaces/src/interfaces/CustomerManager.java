cff6a664b591260c41f39fefddab3eb8a50f8c83
denizaslan41 <deniz.aslan@yilport.com>
2021-05-01 23:50:17 +0300

    010520212350

--- interfaces/src/interfaces/CustomerManager.java
+++ interfaces/src/interfaces/CustomerManager.java
@@ -9,24 +9,14 @@ public class CustomerManager {
 		this.loggers = loggers;
 	}
 	
-	
 	public void add(Customer customer) {
-		System.out.println("Müþteri eklendi " + customer.getFirstname());
-		
-		for(Logger logger : loggers) {
-			logger.Log(customer.getFirstname());
-		}
-		
-		
+		System.out.println("Müþteri eklendi " + customer.getFirstname());		
+		Utils.runLoggers(loggers, customer.getFirstname());		
 	}
 	
+		
 		public void delete(Customer customer) {
-		System.out.println("Müþteri silindi " + customer.getFirstname());
-		
-		for(Logger logger : loggers) {
-			logger.Log(customer.getFirstname());
-		}
-		
-	}
-	
+		System.out.println("Müþteri silindi " + customer.getFirstname());		
+		Utils.runLoggers(loggers, customer.getLastName());		
+	}	
 }
